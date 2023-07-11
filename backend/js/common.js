72f5bf6099c783981d8d865765addf6c23f6377d
izumikana <k.izumi@jin-it.co.jp>
2023-07-11 14:54:54 +0900

    モーダル追加

--- backend/js/common.js
+++ backend/js/common.js
@@ -1,5 +1,42 @@
-function calculateBet(amount) {
-        var currentBet = parseInt(document.getElementById("bet-amount").innerHTML.replace("$", ""));
-        var newBet = currentBet + parseInt(amount);
-        document.getElementById("bet-amount").innerHTML = '$' + newBet;
-    }
+$(document).ready(function() {
+    // BET、クレジット表示
+    var initialBet = parseInt($('.js-bet span').text());//初期ベット金額取得
+
+    $('.coin-input').click(function() {
+        var coinValue = parseInt($(this).val());//選択コイン額
+        var currentBet = parseInt($('.js-bet span').text());//現在のベット金額の取得
+        var currentCredit = parseInt($('.js-credit span').text());//現在のクレジット金額の取得
+
+        if ($(this).is(':checked')) {
+            if (currentCredit >= coinValue + initialBet) {
+                var newBet = currentBet + coinValue;//現在ベット額＋選択コイン額
+                var newCredit = currentCredit - coinValue;
+                $('.js-bet span').text(newBet);
+                $('.js-credit span').text(newCredit);
+            } else {
+                $('.js-credit').text('クレジットが不足しています');
+                $(this).prop('checked', false);
+            }
+        } else {
+            var newBet = currentBet - coinValue;
+            var newCredit = currentCredit + coinValue;
+            $('.js-bet span').text(newBet);
+            $('.js-credit span').text(newCredit);
+            $('.js-credit').text('＄' + newCredit);
+        }
+    });
+    // モーダルウィンドウ
+    $('.js-open').on('click', function() {
+        $('.js-modal').addClass('open');
+        $('.js-overlay').addClass('open');
+        return false; // フォームのサブミットを防止
+    });
+    $('.js-close').on('click', function() {
+        $('.js-modal').removeClass('open');
+        $('.js-overlay').removeClass('open');
+    });
+});
+
+
+
+
--- backend/php/views/home.php
+++ backend/php/views/home.php
@@ -1,65 +1,83 @@
 <?php
-$a = 10;
-$b = 50;
-$c = 100;
-$d = 500;
-$bet = $a + $b + $c + $d;
-$credit = 200 + $bet;
+$a = 10; // コイン10
+$b = 50; // コイン50
+$c = 100; // コイン100
+$d = 500; // コイン500
+
+$bet = 0; // ベットの初期値
+$credit = 200; // クレジットの初期値
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST['coin'])) {
+        $selectedCoins = $_POST['coin']; // 選択されたコインの配列
+        foreach ($selectedCoins as $coin) {
+            $coinValue = (int)$coin;
+            $bet += $coinValue; // ベットに選択されたコインの金額を加算
+            $credit -= $coinValue; // クレジットから選択されたコインの金額を減算
+        }
+    }
+}
 ?>
 
 <section id="home">
     <div class="bg">
         <div class="unit">
             <h1>BLACKJACK</h1>
-            <form action="" method="">
+            <form action="" method="post">
                 <div class="name">
                     <input type="text" value="" name="name" class="textbox" placeholder="プレイヤー名を入力してください。">
                 </div>
-                <div class="btn">
-                    <input type="submit" value="PLAY">
+                <div class="btn js-open">
+                    <input type="submit" value="PLAY" class="play">
                 </div>
             </form>
         </div>
     </div>
-    <!-- オーバーレイ -->
-    <div id="overlay" class="overlay"></div>
-    <!-- モーダルウィンドウ -->
-    <div class="modal-window">
-        <!-- 閉じるボタン -->
-        <button class="js-close button-close">Close</button>
-        <!-- 内容 -->
-        <div class="content">
-            <p>BETする金額を決めてください。</p>
-            <div class="coin">
-                <div class="item">
-                    <label for="input-a">
-                        <img src="../../img/img_coin-10.png" alt="">
-                    </label>
-                    <input id="input-a" type="hidden" name="10" value="<?php echo $a; ?>" onclick="calculateBet(this.value)">
-                </div>
-                <div class="item">
-                    <label for="input-b">
-                        <img src="../../img/img_coin-50.png" alt="">
-                    </label>
-                    <input id="input-b" type="hidden" name="50" value="<?php echo $b; ?>" onclick="calculateBet(this.value)">
-                </div>
-                <div class="item">
-                    <label for="input-c">
-                        <img src="../../img/img_coin-100.png" alt="">
-                    </label>
-                    <input id="input-c" type="hidden" name="100" value="<?php echo $c; ?>" onclick="calculateBet(this.value)">
-                </div>
-                <div class="item">
-                    <label for="input-d">
-                        <img src="../../img/img_coin-500.png" alt="">
-                    </label>
-                    <input id="input-d" type="hidden" name="500" value="<?php echo $d; ?>" onclick="calculateBet(this.value)">
-                </div>
+</section>
+<!-- オーバーレイ -->
+<div class="overlay js-overlay"></div>
+<!-- モーダルウィンドウ -->
+<section class="modal-window js-modal">
+    <!-- 閉じるボタン -->
+    <button class=" button-close js-close">
+        <span></span>
+        <span></span>
+    </button>
+    <!-- 内容 -->
+    <div class="content">
+        <p>BETする金額を決めてください。</p>
+        <div class="coin">
+            <div class="item">
+                <label for="input-a">
+                    <img src="../../img/img_coin-10.png" alt="">
+                    <input type="checkbox" name="coin" value="<?php echo $a; ?>" class="coin-input" id="input-a">
+                </label>
             </div>
-            <div class="flex">
-                <div id="bet-amount" class="bet"><?php echo '$' . $bet; ?></div>
-                <div class="credit"><?php echo '$' . $credit; ?></div>
+            <div class="item">
+                <label for="input-b">
+                    <img src="../../img/img_coin-50.png" alt="">
+                    <input type="checkbox" name="coin" value="<?php echo $b; ?>" class="coin-input" id="input-b">
+                </label>
             </div>
+            <div class="item">
+                <label for="input-c">
+                    <img src="../../img/img_coin-100.png" alt="">
+                    <input type="checkbox" name="coin" value="<?php echo $c; ?>" class="coin-input" id="input-c">
+                </label>
+            </div>
+            <div class="item">
+                <label for="input-d">
+                    <img src="../../img/img_coin-500.png" alt="">
+                    <input type="checkbox" name="coin" value="<?php echo $d; ?>" class="coin-input" id="input-d">
+                </label>
+            </div>
+        </div>
+        <div class="flex">
+            <div class="bet js-bet">＄<span><?php echo $bet; ?></span></div>
+            <div class="credit js-credit">＄<span><?php echo $credit; ?></span></div>
+        </div>
+        <div class="btn">
+            <input type="submit" value="START" class="start">
         </div>
     </div>
 </section>
