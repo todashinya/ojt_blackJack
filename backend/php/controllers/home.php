98419322c1ebd087db7061240cff79119b11b166
todashinya <s.toda@jin-it.co.jp>
2023-07-15 10:11:33 +0900

    ホーム画面からPOSTできるmodalを仮作成

--- backend/php/controllers/home.php
+++ backend/php/controllers/home.php
@@ -1,2 +1,14 @@
 <?php
 require_once SOURCE_PATH . 'views/home.php';
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    echo('<pre>');
+    var_dump($_POST ?? '');
+    echo('</pre>');
+
+    
+
+}
--- backend/php/views/home.php
+++ backend/php/views/home.php
@@ -2,18 +2,42 @@
 use db\DataSource;
 use model\PlayerModel;
 
-try{
-    $db =  new DataSource();
-    $result = $db->select("select * from t_player where id = :id;", [':id' => 2], 'cls', 'model\PlayerModel');  
-    echo('<pre>');
-    var_dump($result);
-    echo('</pre>');
+// try{
+//     $db =  new DataSource();
+//     $result = $db->select("select * from t_player where id = :id;", [':id' => 2], 'cls', 'model\PlayerModel');  
+//     echo('<pre>');
+//     var_dump($result);
+//     echo('</pre>');
 
-} catch(PDOException $e) {
-    echo $e->getMessage();
-}
+// } catch(PDOException $e) {
+//     echo $e->getMessage();
+// }
 
 ?>
 
-<h1>ホーム画面</h1>
-<a href="/game">aaaaa</a>
+<div class="container">
+    <main class="main">
+        <div class="main__inner">
+            <h1>BLACKJACK</h1>
+            <form action="" method="POST">
+                <input type="text" name="data['name']" value="" placeholder="プレイヤー名を入力してください">
+                <button type="submit" id="playBtn">PLAY</button>
+            </form>
+        </div>
+    </main>
+</div>
+
+
+<!-- モーダルのHTML -->
+<div id="modal" class="modal">
+    <div class="modal__content">
+        <span class="modal__close">&times;</span>
+        <h2>BETする金額を決めてください。</h2>
+        <form action="" method="post">
+            <input type="text" name="data['bet']" value="">
+            <input type="text" name="data['credit']" value="">
+            <input type="hidden" name="data['name']" value="">
+            <button type="submit" id="startBtn">START</button>
+        </form>
+    </div>
+</div>
