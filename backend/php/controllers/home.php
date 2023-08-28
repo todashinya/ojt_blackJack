b034c90d602d991da310d31caf22567170051bf6
izumikana <k.izumi@jin-it.co.jp>
2023-08-28 09:40:34 +0900

    controllers/homeにクラス追加

--- backend/php/controllers/home.php
+++ backend/php/controllers/home.php
@@ -10,20 +10,25 @@ require_once SOURCE_PATH . 'views/home.php';
 class HomeController
 {
 
-    public function register($data)
+    public function register()
     {
-        $db = new PlayerQuery();
-        $db->addPlayer($data);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        $data[] = array(
-            "bet" => $_POST['bet'],
-            "credit" => $_POST['credit'],
-            "name" => $_POST['name'],
-            "startDate" => date('Y-m-d H:i:s')
-        );
+            $data = [];
+            $data[] = array(
+                "bet" => $_POST['bet'],
+                "credit" => $_POST['credit'],
+                "name" => $_POST['name'],
+                "startDate" => date('Y-m-d H:i:s')
+            );
+
+            $db = new PlayerQuery();
+            $db->addPlayer($data);
+        }
     }
 }
-
+$homeController = new HomeController();
+$homeController->register();
 
 
 
